0790b44dc895be547d8f12b2a63ae438b461a289
Ryan Gomes Martins <ryteckgm@gmail.com>
2025-02-19 18:54:30 -0300

    nlw react 2

---
+++ react/src/app/invite/invite-link-input.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { CopyIcon, LinkIcon } from "lucide-react";
+
+interface InviteLinkInputProps {
+	inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+	function copyInviteLink() {
+		navigator.clipboard.writeText(inviteLink);
+	}
+
+	return (
+		<InputRoot>
+			<InputIcon>
+				<LinkIcon className="size-5" />
+			</InputIcon>
+
+			<InputField readOnly defaultValue={inviteLink} />
+
+			<IconButton className="-mr-2" onClick={copyInviteLink}>
+				<CopyIcon className="size-5" />
+			</IconButton>
+		</InputRoot>
+	);
+}
