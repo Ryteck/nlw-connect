eec85595ddcde39161b7b22f496220a5b4b50b37
Ryan Gomes Martins <ryteckgm@gmail.com>
2025-02-19 10:50:38 -0300

    nlw react 1

---
+++ react/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Montserrat, Oxanium } from "next/font/google";
+
+export const metadata: Metadata = {
+	title: "devstage",
+};
+
+const montserrat = Montserrat({
+	weight: ["400", "600"],
+	subsets: ["latin"],
+	variable: "--font-montserrat",
+});
+
+const oxanium = Oxanium({
+	weight: ["500", "600"],
+	subsets: ["latin"],
+	variable: "--font-oxanium",
+});
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="pt-BR" className={`${montserrat.variable} ${oxanium.variable}`}>
+			<body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+				{children}
+			</body>
+		</html>
+	);
+}
